e7650656c9f5b4447b3b429327e3373255b48a96
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-02-08 14:22:24 +0000

    Add user model, controller, actions(todo) and new user view

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -16,6 +16,7 @@ class Admin::CategoriesController < ApplicationController
       redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
+      
     end
   end
 
